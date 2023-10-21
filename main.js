decb227a1d1a2310df63d15310a5a5a45f6d1c44
chojeongeun <juy0816@naver.com>
2023-10-21 10:44:36 +0900

    12시가 넘어가면 pm값 출력 및 12시 빼주기

--- main.js
+++ main.js
@@ -1,28 +1,10 @@
 //비구조화 할당
 const numbers = document.querySelectorAll('.screen span');
-//const [spanHr, spanMin, spanSec]  = document.querySelectorAll('.screen span');
+const [am, pm] = document.querySelectorAll('.screen em');
 
 setInterval(() => {
 	getTime().forEach((num, idx) => setTime(num, idx));
 }, 1000);
-
-// setInterval(() => {
-// 	const now = new Date();
-// 	let hr = now.getHours();
-// 	let min = now.getMinutes();
-// 	let sec = now.getSeconds();
-
-// 	if (hr < 10) hr = '0' + hr;
-// 	if (min < 10) min = '0' + min;
-// 	if (sec < 10) sec = '0' + sec;
-
-// 	numbers[0].innerText = hr;
-// 	numbers[1].innerText = min;
-// 	numbers[2].innerText = sec;
-//   // spanHr.innerText = hr;
-// 	// spanMin.innerText = min;
-// 	// spanSec.innerText = sec;
-// }, 1000);
 
 //시간값을 구해서 반환하는 함수
 function getTime() {
@@ -30,7 +12,15 @@ function getTime() {
 	let hr = now.getHours();
 	let min = now.getMinutes();
 	let sec = now.getSeconds();
-	return [hr, min, sec];
+
+	if (hr >= 12) {
+		am.classList.remove('on');
+		pm.classList.add('on');
+		hr = hr - 12;
+	} else {
+		pm.classList.remove('on');
+		am.classList.add('on');
+	}
 }
 
 //반환된 시간값을 DOM에 세팅하는 함수
