2dfc4c2336081146b9c75e2742a517fa6ddfda74
chojeongeun <juy0816@naver.com>
2023-10-21 09:42:45 +0900

    시간값 출력

--- main.js
+++ main.js
@@ -1,27 +1,26 @@
-/*
-  생성자 함수
-  - 객체 복사본을 생성해주는 함수
-
-  인스턴스 (객체)
-  - 생성자 함수를 통해서 복사가 된 객체
-
-  프로토타입(생성자를 통해 만들어진 객체 복사본들이 공유하는 공통의 저장공간)
-  - 각각의 인스턴스 객체들이 공유할 수 있는 함수들이 등록되는 공간
-*/
-const monthName = ['January', 'February', 'March,', 'Aprill', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
-
-const dayName = ['sunday', 'monday', 'tueswday', 'wednseday', 'thursday', 'friday', 'saturday'];
+//우리나라 시간대 값을 활용해서 현재 시간값을 출력, 12시가 넘어가면 시간값 앞에 pm을 붙이고 12시 이전이면 am을 붙이기
+const title = document.querySelector('h1');
+const [em, spanHr, spanMin, spanSec] = title.children;
+console.log(em);
 
 const now = new Date();
-const year = now.getFullYear();
-const month = now.getMonth();
-const date = now.getDate();
-const day = now.getDay();
-const hour = now.getHours();
+const hr = now.getHours();
 const min = now.getMinutes();
 const sec = now.getSeconds();
 
-//우리나라 시간대를 전세계 표준시로 변환해서 반환하는 것이 toGMTString
+let apm = hr < 12 ? 'am' : 'pm';
+/*
+if (hr < 12) {
+	apm = 'am';
+} else {
+	apm = 'pm';
+}
+*/
 
-const gmtNow = now.toUTCString();
-console.log(gmtNow);
+//위는 복잡하니까 삼항연산자로 아래처럼
+// hr < 12 ? (apm = 'am') : (apm = 'pm');
+
+em.innerText = apm;
+spanHr.innerText = hr;
+spanMin.innerText = min;
+spanSec.innerText = sec;
