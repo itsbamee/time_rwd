0f8f5d3215e65f42cda4c6bcb96206c9116e670a
chojeongeun <juy0816@naver.com>
2023-10-21 10:23:53 +0900

    시간함수 코드정리

--- main.js
+++ main.js
@@ -1,34 +1,40 @@
-//우리나라 시간대 값을 활용해서 현재 시간값을 출력, 12시가 넘어가면 시간값 앞에 pm을 붙이고 12시 이전이면 am을 붙이기
-const title = document.querySelector('h1');
-const [em, spanHr, spanMin, spanSec] = title.children;
-console.log(em);
-
-/*
-if (hr < 12) {
-	apm = 'am';
-} else {
-	apm = 'pm';
-}
-*/
-
-//위는 복잡하니까 삼항연산자로 아래처럼
-// hr < 12 ? (apm = 'am') : (apm = 'pm');
-
-// spanHr.innerText = hr;
-// spanMin.innerText = min;
-// spanSec.innerText = sec;
-
-//각각의 시간, 분, 초에서 한자리 숫자일때는 앞에 0을 붙여서 출력
+//비구조화 할당
+const numbers = document.querySelectorAll('.screen span');
+//const [spanHr, spanMin, spanSec]  = document.querySelectorAll('.screen span');
 
 setInterval(() => {
-	const now = new Date();
-	const hr = now.getHours();
-	const min = now.getMinutes();
-	const sec = now.getSeconds();
-	let apm = hr < 12 ? 'am' : 'pm';
-	em.innerText = apm;
-
-	spanHr.innerText = hr < 10 ? '0' + hr : hr;
-	spanMin.innerText = min < 10 ? '0' + min : min;
-	spanSec.innerText = sec < 10 ? '0' + sec : sec;
+	getTime().forEach((num, idx) => setTime(num, idx));
 }, 1000);
+
+// setInterval(() => {
+// 	const now = new Date();
+// 	let hr = now.getHours();
+// 	let min = now.getMinutes();
+// 	let sec = now.getSeconds();
+
+// 	if (hr < 10) hr = '0' + hr;
+// 	if (min < 10) min = '0' + min;
+// 	if (sec < 10) sec = '0' + sec;
+
+// 	numbers[0].innerText = hr;
+// 	numbers[1].innerText = min;
+// 	numbers[2].innerText = sec;
+//   // spanHr.innerText = hr;
+// 	// spanMin.innerText = min;
+// 	// spanSec.innerText = sec;
+// }, 1000);
+
+//시간값을 구해서 반환하는 함수
+function getTime() {
+	const now = new Date();
+	let hr = now.getHours();
+	let min = now.getMinutes();
+	let sec = now.getSeconds();
+	return [hr, min, sec];
+}
+
+//반환된 시간값을 DOM에 세팅하는 함수
+function setTime(num, index) {
+	num = num < 10 ? (num = '0' + num) : num;
+	numbers[index].innerText = num;
+}
